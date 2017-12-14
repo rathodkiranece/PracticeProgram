531b16921ae40b4a8f6e3cdec069bcbe01efe04f
Admin <Admin@192.168.0.2>
2017-12-15 01:00:11 +0530

    First change

--- CODEC/src/com/app/Encoding.java
+++ CODEC/src/com/app/Encoding.java
@@ -1,17 +1,15 @@
 package com.app;
 
 import org.apache.commons.codec.binary.Base64;
-
 public class Encoding {
 	public static void main(String[] args) {
 	//ENCODING
-		
 		String msg="kiran";
 		byte[] a=msg.getBytes();
 		byte[] b=Base64.encodeBase64(a);
 		String enc=new String(b);
 		System.out.println(enc);
-		//DECODING for String
+		//DECODING
 		byte[] c=enc.getBytes();
 		byte[] d=Base64.decodeBase64(c);
 		String nrml=new String(d);
