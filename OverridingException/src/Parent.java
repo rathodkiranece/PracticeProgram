087d2be9129e514601efa878813141034ecedaa1
rathodkiranece <rathodkiranece@gmail.com>
2019-02-17 20:56:40 +0530

    OverridingException

---
+++ OverridingException/src/Parent.java
@@ -0,0 +1,13 @@
+class Parent{  
+  void msg(){System.out.println("parent");}  
+}  
+  
+class TestExceptionChild1 extends Parent{  
+  void msg()throws ArithmeticException{  
+    System.out.println("child");  
+  }  
+  public static void main(String args[]){  
+   Parent p=new TestExceptionChild1();  
+   p.msg();  
+  }  
+}  
